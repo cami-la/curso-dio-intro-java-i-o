1ee5200187c2e216375b0942e65294039076e2c8
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2021-04-03 07:23:11 -0300

    exemplo try-catch-finally

--- src/br/com/dio/java/io/IOException/ExemploTryCatchFinally.java
+++ src/br/com/dio/java/io/IOException/ExemploTryCatchFinally.java
@@ -1,4 +1,33 @@
 package br.com.dio.java.io.IOException;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class ExemploTryCatchFinally {
+    public static void main(String[] args) {
+        File f = new File("dica-para-ler-e-escrever-arquivo-java-IO.txt"); //arquivo existe
+        File f1 = new File("test.txt"); //arquivo não existe
+        FileReader fr = null; //serve para ler um stream de caracter
+        try { //tente receber o stream f e f1
+            fr = new FileReader(f.getName()); //recebe o stream. Sendo positivo, o try segue sendo executado.
+            System.out.println("Stream recebido com sucesso! " + f.getName());
+            fr.close(); //fechamos o stream.
+
+            fr = new FileReader(f1.getName()); //esse arquivo não existe, logo o try é interrompido e vai para o catch.
+            System.out.println("Stream recebido com sucesso! " + f.getName());
+            fr.close();
+        } catch (FileNotFoundException e) { //se o arquivo não existe
+            System.out.println("Arquivo não encontrado, motivo: " + e.getCause()); //mostra a causa
+            System.out.println("Por gentileza, confira se o arquivo existe " + e.getMessage()); //mostra o nome
+            //e.printStackTrace();
+        } catch (IOException | NullPointerException e) { //caso o stream fosse impedido de ser fechado
+            System.out.println("Arquivo não pode ser fechado!" + e.getCause()); //mostra a causa
+        } finally { //(opcional)
+            System.out.println("A execução do finally independe se apresentou exception ou não!");
+        }
+        System.out.println("** Programa continua normalmente! **"); //apesar de ter apresentado uma exception.
+        //caso tivesse outro try por aqui ou qualquer outra rotina, seria executado!
+    }
 }
