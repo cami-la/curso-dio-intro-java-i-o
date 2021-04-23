57887aed1c59fb1be21e04f2551caf240d24390f
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2021-04-23 03:57:58 -0300

    Exercicio2 IOBytes Final

--- src/br/com/dio/java/io/IOBytes/ExemploIOBytes.java
+++ src/br/com/dio/java/io/IOBytes/ExemploIOBytes.java
@@ -1,6 +1,39 @@
 package br.com.dio.java.io.IOBytes;
 
+import java.io.*;
+
 //Faça uma cópia do arquivo “recomendacoes-copy.txt”.
 public class ExemploIOBytes {
+    public static void copiarArquivo() throws IOException {
+        File f = new File("/home/cami/git/curso-dio-intro-java-io/recomendacoes-copy.txt");
+        String nomeArquivo = f.getName();
 
+        /*InputStream is = new FileInputStream(f.getName());
+        BufferedInputStream bis = new BufferedInputStream(is);*/
+        BufferedInputStream bis = new BufferedInputStream(new FileInputStream(f.getName()));
+
+        String nomeArquivoCopy = nomeArquivo.substring(0, nomeArquivo.indexOf("-")).concat("-copy2.txt");
+        File fcopy = new File(nomeArquivoCopy);
+
+        /*OutputStream os = new FileOutputStream(nomeArquivoCopy);
+        BufferedOutputStream bos = new BufferedOutputStream(os);*/
+        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(nomeArquivoCopy));
+
+        int line = 0;
+        while((line = bis.read()) != -1) {
+            bos.write((char) line);
+            bos.flush();
+        }
+
+        bis.close();
+        bos.close();
+
+        PrintStream ps = new PrintStream(System.out);
+        ps.printf("Arquivo %s copiado com sucesso e está no diretório %s com %d bytes ",
+                fcopy.getName(), fcopy.getPath(), fcopy.length());
+    }
+
+    public static void main(String[] args) throws IOException {
+        copiarArquivo();
+    }
 }
--- src/br/com/dio/java/io/IOCharacter/Exercicio3IOCaracter.java
+++ src/br/com/dio/java/io/IOCharacter/Exercicio3IOCaracter.java
@@ -1,6 +1,67 @@
 package br.com.dio.java.io.IOCharacter;
 
+
+import java.io.*;
+
 //Faça uma cópia do arquivo “recomendacoes.txt” e agora adicione 3 recomendações de livros.
 public class Exercicio3IOCaracter {
+    public static void copiarArquivo() throws IOException {
+        File f = new File("/home/cami/git/curso-dio-intro-java-io/recomendacoes.txt");
+        String nameArquivo = f.getName();
 
+        /*FileReader r = new FileReader(nameArquivo);
+        BufferedReader br = new BufferedReader(r);*/
+        BufferedReader br = new BufferedReader(new FileReader(nameArquivo));
+        String line = br.readLine();
+
+        String nameArquivoCopy = nameArquivo.substring(0, nameArquivo.indexOf(".")).concat("-copy.txt");
+        File fcopy = new File(nameArquivoCopy);
+
+        BufferedWriter bw = new BufferedWriter(new FileWriter(fcopy.getName()));
+
+        do{
+            bw.write(line);
+            bw.newLine();
+            bw.flush();
+            line = br.readLine();
+        }while(!(line == null));
+
+        System.out.printf("Arquivo \"%s\" copiado com sucesso! Com o tamanho '%d' bytes.\n", f.getName(), f.length());
+        System.out.printf("Arquivo \"%s\" criado com sucesso! Com o tamanho '%d' bytes.\n", fcopy.getName(), fcopy.length());
+
+        PrintWriter pw = new PrintWriter(System.out);
+        pw.println("Recomende 3 livros: ");
+        pw.flush();
+
+        adicionarInfoNoArquivo(fcopy.getName());
+
+        pw.printf("Ok! Tudo certo. Tamanho do arquivo '%d' bytes.", fcopy.length());
+
+        br.close();
+        bw.close();
+        pw.close();
+
+
+    }
+
+    public static void adicionarInfoNoArquivo(String arquivo) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = br.readLine();
+
+        BufferedWriter bw = new BufferedWriter(new FileWriter(arquivo, true));
+
+        do{
+            bw.write(line);
+            bw.newLine();
+            line = br.readLine();
+        }while(!(line.equalsIgnoreCase("fim")));
+
+        br.close();
+        bw.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        copiarArquivo();
+
+    }
 }
